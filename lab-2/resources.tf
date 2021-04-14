3e1dbebe77ccdfa27b4d2146bb2c63f0e769bb97
Aschay <ayadi.chayma@gmail.com>
2021-04-14 16:24:27 +0100

    add more resource use tfvars

---
+++ lab-2/resources.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+  region     = var.AWS_REGION
+}
+
+resource "aws_instance" "appserver" {
+  count = 2 
+  ami           = lookup(var.AMIS, var.AWS_REGION)
+  instance_type = "t2.micro"
+  tags = {
+    Name = "AppSever-${count.index}"
+  }
+  
+  security_groups = var.Security_Group
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.base_cidr_block
+}
+
+resource "aws_subnet" "az" {
+  count = length(var.availability_zones)
+  availability_zone = var.availability_zones[count.index]
+  vpc_id = aws_vpc.main.id
+  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 4, count.index+1)
+}
---
+++ lab-2/variables.tf
@@ -0,0 +1,37 @@
+variable "AWS_ACCESS_KEY" {
+  description = "AWS access key "
+  type        = string
+  sensitive   = true
+}
+variable "AWS_SECRET_KEY" {
+  description = "AWS secret key "
+  type        = string
+  sensitive   = true
+}
+variable "AWS_REGION" {
+  default = "us-east-2"
+}
+variable "Security_Group" {
+  type    = list(any)
+  default = ["sg-24076", "sg-90890", "sg-456789"]
+}
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-1 = "ami-0f40c8f97004632f9"
+    us-east-2 = "ami-05692172625678b4e"
+    us-west-2 = "ami-0352d5a37fb4f603f"
+    us-west-1 = "ami-0f40c8f97004632f9"
+  }
+}
+
+variable "base_cidr_block" {
+  description = "A /16 CIDR range definition, such as 10.1.0.0/16, that the VPC will use"
+  default     = "10.1.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "A list of availability zones in which to create subnets"
+  type        = list(string)
+  default     = ["us-east-2a"]
+}
