5d4617eee4172e3a9e15568ec75d1fa56c970266
Aschay <ayadi.chayma@gmail.com>
2021-04-22 16:41:41 +0100

    add basic resource

---
+++ lab-7/resources.tf
@@ -0,0 +1,11 @@
+//create the instance
+resource "aws_instance" "appserver" {
+  ami           = lookup(var.AMIS, var.AWS_REGION)
+  instance_type = "t2.micro"
+  tags = {
+    Name = "AppServer"
+  }
+}
+
+
+
